6ed72a156836cd1b4c376211f646be998e9ded48
Nagendra M <nagendra@Nagendras-MacBook-Air.local>
2025-07-26 23:57:55 +0530

    Made changes to connect to backend

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export default class ApiService {
 
-    static BASE_URL = "http://localhost:4040"
+    static BASE_URL = import.meta.env.VITE_BASE_URL;
 
     static getHeader() {
         const token = localStorage.getItem("token");
